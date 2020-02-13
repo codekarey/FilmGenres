7a72fb78db6f76ba04cd395c380f9f752d9c5391
codekarey <codekarey@gmail.com>
2020-02-13 10:47:40 -0500

    Outline and class

---
+++ FilmGenres/Film.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FilmGenres
+{
+    class Film
+    {
+        //data required to create film
+        private string title;
+        private string category;
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+        public string Category
+        {
+            get
+            {
+                return category;
+            }
+            set
+            {
+                category = value;
+            }
+        }
+        //film item
+        public Film(string _title, string _category)
+        {
+            Title = _title;
+            Category = _category;
+        }
+        //list of flims with genres
+        public static void All()
+        {
+            List<Film> listAll = new List<Film>
+            {
+            new Film("Spirited Away", "Animated"),
+            new Film("Waking Life", "Animated"),
+            new Film("Eternal Sunshine of the Spotless Mind", "Drama"),
+            new Film("Pay It Forward", "Drama"),
+            new Film("IT", "Horror"),
+            new Film("Chucky", "Horror"),
+            new Film("The Gremlins", "Horror"),
+            new Film("The Matrix", "Scifi"),
+            new Film("Back To The Future", "Scifi"),
+            new Film("Shawn Of The Dead", "Scifi"),
+            };
+            //list all films a -> z
+            //  (listAll.OrderBy(x=>x)).ToList();
+            foreach (Film azList in listAll)
+            {
+                Console.WriteLine(azList);
+            }
+        }
+
+    }
+}
---
+++ FilmGenres/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace FilmGenres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool yes = true;
+            while (true)
+            {
+                Console.WriteLine("Enter the number below the film options to continue.");
+                Console.WriteLine(" [   1   ]   [  2  ]   [  3  ]    [  4  ]    [  5   ]    [   6   ]");
+                Console.WriteLine("  Animated    Drama     Horror     SciFi     List All     Add New");
+                try
+                {
+                    //check for valid input
+                    int userPicked = int.Parse(Console.ReadLine());
+                    //menu options
+                    switch{
+                        case "1":
+                            //list all m in 1
+                            break;
+                        case "2":
+                            break;
+                        case "3":
+                            break;
+                        case "4":
+                            break;
+                        case "5":
+                            break;
+                        case "6":
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                catch
+                {
+                    //error message
+                    Console.WriteLine("Sorry, please try a number 1-6");
+                }
+                //ask if user wants to return to menu or exit
+            }
+        }
+        //display updated film options to console
+        public static void View(List<Film> Genre)
+        {
+            foreach (Film m in Genre)
+            {
+                Console.WriteLine(m);
+            }
+        }
+
+    }
+}
