8738f380c35adfd405f0b4789c4f4b6fcad88d03
codekarey <codekarey@gmail.com>
2020-02-14 13:58:12 -0500

    Working options- before cleanup

--- FilmGenres/Film.cs
+++ FilmGenres/Film.cs
@@ -4,63 +4,29 @@ using System.Text;
 
 namespace FilmGenres
 {
-    class Film
+    public class Film
     {
         //data required to create film
         private string title;
         private string category;
 
-        public string Title
+        public string Title { get => title; set => title = value; }
+        public string Category{ get => category; set => category = value; }
+
+        public Film()
         {
-            get
-            {
-                return title;
-            }
-            set
-            {
-                title = value;
-            }
+
         }
-        public string Category
+        public Film(string title, string category)
         {
-            get
-            {
-                return category;
-            }
-            set
-            {
-                category = value;
-            }
-        }
-        //film item
-        public Film(string _title, string _category)
-        {
-            Title = _title;
-            Category = _category;
-        }
-        //list of flims with genres
-        public static void All()
-        {
-            List<Film> listAll = new List<Film>
-            {
-            new Film("Spirited Away", "Animated"),
-            new Film("Waking Life", "Animated"),
-            new Film("Eternal Sunshine of the Spotless Mind", "Drama"),
-            new Film("Pay It Forward", "Drama"),
-            new Film("IT", "Horror"),
-            new Film("Chucky", "Horror"),
-            new Film("The Gremlins", "Horror"),
-            new Film("The Matrix", "Scifi"),
-            new Film("Back To The Future", "Scifi"),
-            new Film("Shawn Of The Dead", "Scifi"),
-            };
-            //list all films a -> z
-            //  (listAll.OrderBy(x=>x)).ToList();
-            foreach (Film azList in listAll)
-            {
-                Console.WriteLine(azList);
-            }
+            Title = title;
+            Category = category;
         }
 
+        //orders how info is printed
+        public override string ToString()
+        {
+            return $"|\t{Category}\t|\t{Title}\t|";
+        }
     }
 }
--- FilmGenres/Program.cs
+++ FilmGenres/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FilmGenres
 {
@@ -7,33 +9,57 @@ namespace FilmGenres
         static void Main(string[] args)
         {
             bool yes = true;
-            while (true)
+            while (yes)
             {
                 Console.WriteLine("Enter the number below the film options to continue.");
                 Console.WriteLine(" [   1   ]   [  2  ]   [  3  ]    [  4  ]    [  5   ]    [   6   ]");
                 Console.WriteLine("  Animated    Drama     Horror     SciFi     List All     Add New");
+                string input=Console.ReadLine();
                 try
                 {
-                    //check for valid input
-                    int userPicked = int.Parse(Console.ReadLine());
-                    //menu options
-                    switch{
+                    //check for valid input and nav menu options
+                    switch(input){
                         case "1":
                             //list all m in 1
+                            List<Film> anim = new List<Film>
+                            {
+                              new Film("Spirited Away", "Animated"),
+                              new Film("Waking Life", "Animated"),
+                            };
+                            View(anim);
                             break;
                         case "2":
+                            foreach(Film drama in listAll)
+                            {
+                                if (drama.Category == "Drama")
+                                {
+                                Console.WriteLine(drama);
+                                }
+                                 
+                            }
+                           
                             break;
                         case "3":
+
                             break;
                         case "4":
                             break;
                         case "5":
+                            //sends to view format in Film to print public list
+                             View(listAll);
                             break;
                         case "6":
-                            break;
+                            Console.WriteLine("Enter the Genre:");
+                            string newG = Console.ReadLine();
+                            Console.WriteLine("Enter the Title:");
+                            string newT = Console.ReadLine();
+                            Film userFilm = new Film{ Title = newT, Category = newG};
+                            listAll.Add(userFilm);
+                            View(listAll);
+                    break;
                         default:
                             break;
-                    }
+                    };
                 }
                 catch
                 {
@@ -46,11 +72,25 @@ namespace FilmGenres
         //display updated film options to console
         public static void View(List<Film> Genre)
         {
+            Console.WriteLine("\t   Genre\t     Title\n*************************************************");
             foreach (Film m in Genre)
             {
-                Console.WriteLine(m);
+                Console.WriteLine(m+"\n-\t-\t-\t-\t-\t-\t-");
             }
         }
-
+        //sample film list
+        public static List<Film> listAll = new List<Film>
+            {
+            new Film("Spirited Away", "Animated"),
+            new Film("Waking Life", "Animated"),
+            new Film("Eternal Sunshine of the Spotless Mind", "Drama"),
+            new Film("Pay It Forward", "Drama"),
+            new Film("IT", "Horror"),
+            new Film("Chucky", "Horror"),
+            new Film("The Gremlins", "Horror"),
+            new Film("The Matrix", "Scifi"),
+            new Film("Back To The Future", "Scifi"),
+            new Film("Shawn Of The Dead", "Scifi"),
+            };
     }
 }
